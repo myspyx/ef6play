363277289e8758c5b581b06657703a0492124002
Bill.Zhuang <zhuangmin@gmail.com>
2021-11-14 20:52:59 +0800

    Init commit

---
+++ EF6Play.Host/BlogDbContext.cs
@@ -0,0 +1,24 @@
+﻿using System.Data.Entity;
+
+namespace EF6Play.Host
+{
+    public class BlogDbContext : DbContext
+    {
+        public BlogDbContext(): base("BlogDbContext")
+        {
+            
+        }
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Post> Posts { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Blog>()
+                .HasMany(b => b.Posts)
+                .WithRequired(p => p.Blog);
+            
+            /*modelBuilder.HasDefaultSchema("public");
+            base.OnModelCreating(modelBuilder);*/
+        }
+    }
+}
---
+++ EF6Play.Host/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace EF6Play.Host
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            using (var db = new BlogDbContext())
+            {
+                var blog = new Blog { Name = "the little scheme" };
+                db.Blogs.Add(blog);
+                db.SaveChanges();
+
+                // Display all Blogs from the database
+                var query = from b in db.Blogs
+                    orderby b.Name
+                    select b;
+
+                Console.WriteLine("All blogs in the database:");
+                foreach (var item in query)
+                {
+                    Console.WriteLine(item.Name);
+                }
+
+                Console.WriteLine("Press any key to exit...");
+                Console.ReadKey();
+            }
+        }
+    }
+    
+    
+}
